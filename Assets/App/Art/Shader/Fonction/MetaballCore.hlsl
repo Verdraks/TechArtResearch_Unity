ea0883a9a4d6908a2477f076b5b56b0404135279
Verdraks <martinezorlando955@gmail.com>
2025-10-07 14:22:43 +0200

    Feat: Id data fill buffer WIP

--- Assets/App/Art/Shader/Fonction/MetaballCore.hlsl
+++ Assets/App/Art/Shader/Fonction/MetaballCore.hlsl
@@ -6,7 +6,7 @@
 StructuredBuffer<MetaballData> _MetaballsDataBuffer;
 int _MetaballsCount;
 
-void GetDistanceMetaball_float(float3 p, out float distance)
+float GetDistanceMetaball_float(float3 p)
 {
     float sumDensity = 0.0;
     float sumRi = 0.0;
@@ -15,7 +15,7 @@ void GetDistanceMetaball_float(float3 p, out float distance)
     for (int i = 0; i < _MetaballsCount; i++)
     {
         float3 center = _MetaballsDataBuffer[i].Position;
-        float radius = 1;
+        float radius = _MetaballsDataBuffer[i].Radius;
         float r = length(p - center);
         if (r < radius)
         {
@@ -24,48 +24,57 @@ void GetDistanceMetaball_float(float3 p, out float distance)
         minDist = min(minDist, r-radius);
         sumRi += radius;
     }
-    distance = max(minDist, (0.2 - sumDensity) / (3.0 / 2.0 * sumRi));
+    return  max(minDist, (0.2 - sumDensity) / (3.0 / 2.0 * sumRi));
 }
+
+float GetDistanceSphere_float(float3 p, float3 center, float radius)
+{
+    return length(p - center) - radius;
+}
+
 
 void SphereTraceMetaballs_float(float3 worldPosition, float3 viewPosition, out float alpha)
 {
     #if defined(SHADERGRAPH_PREVIEW)
     alpha = 1.0;
     #else
-
-    
-        float3 c = float3(0,0,0);
-        float dist = length(worldPosition - c);
-        alpha = saturate(1.0 - dist / 2.0); // fade autour du premier metaball
-        return;
     
     float maxDist = 100.0;
     float threshold = 0.0001;
     float t = 0.0;
     int numSteps = 0;
     
-    half3 viewDir = normalize(viewPosition - worldPosition);
+    half3 viewDir = normalize( worldPosition - viewPosition );
 
-    while (t < maxDist)
+    for (int i = 0; i < _MetaballsCount; i++)
     {
-        float minDist = 1000000.0;
-        float3 from = viewPosition + t * viewDir;
-        float d = 0.0;
-        GetDistanceMetaball_float(from,d);
-
-        if (d < minDist)
+        while (t < maxDist)
         {
-            minDist = d;
-        }
+            float minDist = 1000000.0;
+            float3 from = viewPosition + t * viewDir;
 
-        if (minDist <= threshold * t)
-        {
-            alpha = 1.0;
-            break;
-        }
+            float d = 0.0;
+            //Work, data buffer filled
+            d = GetDistanceSphere_float(from, _MetaballsDataBuffer[i].Position, _MetaballsDataBuffer[i].Radius);
 
-        t+= minDist;
-        numSteps++;
+            //Dont work
+            // d = GetDistanceMetaball_float(from);
+        
+
+            if (d < minDist)
+            {
+                minDist = d;
+            }
+
+            if (minDist <= threshold * t)
+            {
+                alpha = 1.0;
+                break;
+            }
+
+            t+= minDist;
+            numSteps++;
+        }
     }
     
     #endif
